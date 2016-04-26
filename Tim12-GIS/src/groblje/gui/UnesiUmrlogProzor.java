7ff14e1cb3ce82c00bef6e9a3c9be66577565ad3
kobacx95 <kobacx95@manojlovic95>
2016-04-27 00:07:47 +0200

    Doradjen UnesiUmrlogProzor i ispravljene greske.

--- Tim12-GIS/src/groblje/gui/UnesiUmrlogProzor.java
+++ Tim12-GIS/src/groblje/gui/UnesiUmrlogProzor.java
@@ -18,6 +18,8 @@ import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import java.awt.Component;
+import javax.swing.JComboBox;
+import javax.swing.SpinnerNumberModel;
 
 public class UnesiUmrlogProzor extends JFrame {
 
@@ -27,12 +29,16 @@ public class UnesiUmrlogProzor extends JFrame {
 	private JLabel lblPosveta;
 	private JTextField textPosveta;
 	private JLabel lblDatumRoenja;
-	private JSpinner spinnerRodjenje;
-	private JLabel lblDatumSmrti;
-	private JSpinner spinnerSmrt;
 	private JButton btnUnesiUmrlog;
 	private JLabel lblRezervisao;
 	private JTextField textRezervisao;
+	private JSpinner spinnerDanR;
+	private JSpinner spinnerMesecR;
+	private JSpinner spinnerGodinaR;
+	private JLabel label;
+	private JSpinner spinnerDanS;
+	private JSpinner spinnerMesecS;
+	private JSpinner spinnerGodS;
 
 	/**
 	 * Create the frame.
@@ -41,11 +47,11 @@ public class UnesiUmrlogProzor extends JFrame {
 		setTitle("Unesi umrlog");
 		setResizable(false);
 		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		setBounds(100, 100, 329, 343);
+		setBounds(100, 100, 337, 343);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
-		contentPane.setLayout(new GridLayout(6, 2, 10, 10));
+		contentPane.setLayout(null);
 		contentPane.add(getLblImeIPrezime());
 		contentPane.add(getTextImePrezime());
 		contentPane.add(getLblPosveta());
@@ -53,20 +59,26 @@ public class UnesiUmrlogProzor extends JFrame {
 		contentPane.add(getLblRezervisao());
 		contentPane.add(getTextRezervisao());
 		contentPane.add(getLblDatumRoenja());
-		contentPane.add(getSpinnerRodjenje());
-		contentPane.add(getLblDatumSmrti());
-		contentPane.add(getSpinnerSmrt());
+		contentPane.add(getSpinnerDanR());
 		contentPane.add(getBtnUnesiUmrlog());
+		contentPane.add(getSpinnerMesecR());
+		contentPane.add(getSpinnerGodinaR());
+		contentPane.add(getLabel());
+		contentPane.add(getSpinnerDanS());
+		contentPane.add(getSpinnerMesecS());
+		contentPane.add(getSpinnerGodS());
 	}
 	private JLabel getLblImeIPrezime() {
 		if (lblImeIPrezime == null) {
 			lblImeIPrezime = new JLabel("Ime i prezime");
+			lblImeIPrezime.setBounds(5, 6, 151, 42);
 		}
 		return lblImeIPrezime;
 	}
 	private JTextField getTextImePrezime() {
 		if (textImePrezime == null) {
 			textImePrezime = new JTextField();
+			textImePrezime.setBounds(166, 6, 164, 42);
 			textImePrezime.setColumns(10);
 		}
 		return textImePrezime;
@@ -74,12 +86,14 @@ public class UnesiUmrlogProzor extends JFrame {
 	private JLabel getLblPosveta() {
 		if (lblPosveta == null) {
 			lblPosveta = new JLabel("Posveta");
+			lblPosveta.setBounds(5, 58, 151, 42);
 		}
 		return lblPosveta;
 	}
 	private JTextField getTextPosveta() {
 		if (textPosveta == null) {
 			textPosveta = new JTextField();
+			textPosveta.setBounds(166, 58, 164, 42);
 			textPosveta.setColumns(10);
 		}
 		return textPosveta;
@@ -87,43 +101,32 @@ public class UnesiUmrlogProzor extends JFrame {
 	private JLabel getLblDatumRoenja() {
 		if (lblDatumRoenja == null) {
 			lblDatumRoenja = new JLabel("Datum ro\u0111enja");
+			lblDatumRoenja.setBounds(5, 162, 151, 42);
 		}
 		return lblDatumRoenja;
-	}
-	private JSpinner getSpinnerRodjenje() {
-		if (spinnerRodjenje == null) {
-			spinnerRodjenje = new JSpinner();
-			spinnerRodjenje.setModel(new SpinnerDateModel(new Date(1461621600000L), null, null, Calendar.DAY_OF_YEAR));
-		}
-		return spinnerRodjenje;
-	}
-	private JLabel getLblDatumSmrti() {
-		if (lblDatumSmrti == null) {
-			lblDatumSmrti = new JLabel("Datum smrti");
-		}
-		return lblDatumSmrti;
-	}
-	private JSpinner getSpinnerSmrt() {
-		if (spinnerSmrt == null) {
-			spinnerSmrt = new JSpinner();
-			spinnerSmrt.setModel(new SpinnerDateModel(new Date(1461621600000L), null, null, Calendar.DAY_OF_YEAR));
-		}
-		return spinnerSmrt;
 	}
 	private JButton getBtnUnesiUmrlog() {
 		if (btnUnesiUmrlog == null) {
 			btnUnesiUmrlog = new JButton("Unesi umrlog");
+			btnUnesiUmrlog.setBounds(5, 266, 151, 37);
 			btnUnesiUmrlog.addActionListener(new ActionListener() {
 				public void actionPerformed(ActionEvent arg0) {
 					String imePrezime = textImePrezime.getText();
 					String posveta = textPosveta.getText();
 					String rezervisao = textRezervisao.getText();
-					try{GregorianCalendar datumRodjenja = (GregorianCalendar) spinnerRodjenje.getValue();
-					GregorianCalendar datumSmrti =  (GregorianCalendar) spinnerSmrt.getValue();
+			
+					int danR = (int) spinnerDanR.getValue();
+					int mesecR = (int) spinnerMesecR.getValue();
+					int godinaR = (int) spinnerGodinaR.getValue();
+					GregorianCalendar datumRodjenja = new GregorianCalendar(godinaR, mesecR, danR);
+					
+					int danS = (int) spinnerDanS.getValue();
+					int mesecS = (int) spinnerMesecS.getValue();
+					int godinaS = (int) spinnerGodS.getValue();
+					GregorianCalendar datumSmrti= new GregorianCalendar(godinaS, mesecS, danS);
 					
 					GUIKontroler.unesiUmrlog(imePrezime, posveta, rezervisao, datumRodjenja, datumSmrti);}
-					catch(RuntimeException r){};
-				}
+					
 			});
 		}
 		return btnUnesiUmrlog;
@@ -131,14 +134,69 @@ public class UnesiUmrlogProzor extends JFrame {
 	private JLabel getLblRezervisao() {
 		if (lblRezervisao == null) {
 			lblRezervisao = new JLabel("Rezervisao");
+			lblRezervisao.setBounds(5, 110, 151, 42);
 		}
 		return lblRezervisao;
 	}
 	private JTextField getTextRezervisao() {
 		if (textRezervisao == null) {
 			textRezervisao = new JTextField();
+			textRezervisao.setBounds(166, 110, 164, 42);
 			textRezervisao.setColumns(10);
 		}
 		return textRezervisao;
+	}
+	private JSpinner getSpinnerDanR() {
+		if (spinnerDanR == null) {
+			spinnerDanR = new JSpinner();
+			spinnerDanR.setModel(new SpinnerNumberModel(1, 1, 31, 1));
+			spinnerDanR.setBounds(166, 162, 48, 42);
+		}
+		return spinnerDanR;
+	}
+	private JSpinner getSpinnerMesecR() {
+		if (spinnerMesecR == null) {
+			spinnerMesecR = new JSpinner();
+			spinnerMesecR.setModel(new SpinnerNumberModel(1, 1, 12, 1));
+			spinnerMesecR.setBounds(224, 162, 48, 42);
+		}
+		return spinnerMesecR;
+	}
+	private JSpinner getSpinnerGodinaR() {
+		if (spinnerGodinaR == null) {
+			spinnerGodinaR = new JSpinner();
+			spinnerGodinaR.setBounds(282, 162, 48, 42);
+		}
+		return spinnerGodinaR;
+	}
+	private JLabel getLabel() {
+		if (label == null) {
+			label = new JLabel("Datum ro\u0111enja");
+			label.setBounds(5, 213, 151, 42);
+		}
+		return label;
+	}
+	private JSpinner getSpinnerDanS() {
+		if (spinnerDanS == null) {
+			spinnerDanS = new JSpinner();
+			spinnerDanS.setModel(new SpinnerNumberModel(1, 1, 31, 1));
+			spinnerDanS.setBounds(166, 213, 48, 42);
+		}
+		return spinnerDanS;
+	}
+	private JSpinner getSpinnerMesecS() {
+		if (spinnerMesecS == null) {
+			spinnerMesecS = new JSpinner();
+			spinnerMesecS.setModel(new SpinnerNumberModel(1, 1, 12, 1));
+			spinnerMesecS.setBounds(224, 213, 48, 42);
+		}
+		return spinnerMesecS;
+	}
+	private JSpinner getSpinnerGodS() {
+		if (spinnerGodS == null) {
+			spinnerGodS = new JSpinner();
+			spinnerGodS.setBounds(282, 213, 48, 42);
+		}
+		return spinnerGodS;
 	}
 }
